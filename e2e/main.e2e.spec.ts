54c6d1875ac2db9d2cab472f3c6ba3567dfde222
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2021-09-17 09:54:24 -0700

    Combined all test cases into a single file as this aprroach seems to be addressing the inconcistency in running e2e tests
    Added a timeout to kubectl wait to give it more time for the pod to be ready
    
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

--- e2e/main.e2e.spec.ts
+++ e2e/main.e2e.spec.ts
@@ -1,23 +1,44 @@
-import path from 'path';
+/**
+ * This file contains tests that require Kubernetes to be running.
+ */
+
 import os from 'os';
+import path from 'path';
 import util from 'util';
 
-import { Application, SpectronClient } from 'spectron';
-import { BrowserWindow } from 'electron';
+import fetch from 'node-fetch';
+import { Application } from 'spectron';
+import * as childProcess from '../src/utils/childProcess';
 import NavBarPage from './pages/navbar';
-import GeneralPage from './pages/general';
-import KubernetesPage from './pages/kubernetes';
-import PortForwardingPage from './pages/portforwarding';
-import ImagesPage from './pages/images';
-import TroubleshootingPage from './pages/troubleshooting';
+
 const electronPath = require('electron');
 
-jest.setTimeout(60_000);
+jest.setTimeout(600_000);
+
+async function tool(tool: string, ...args: string[]): Promise<string> {
+  const srcDir = path.dirname(__dirname);
+  const filename = os.platform().startsWith('win') ? `${ tool }.exe` : tool;
+  const exe = path.join(srcDir, 'resources', os.platform(), 'bin', filename);
+
+  try {
+    const { stdout } = await childProcess.spawnFile(
+      exe, args, { stdio: ['ignore', 'pipe', 'inherit'] });
+
+    return stdout;
+  } catch (ex:any) {
+    console.error(`Error running ${ tool } ${ args.join(' ') }`);
+    console.error(`stdout: ${ ex.stdout }`);
+    console.error(`stderr: ${ ex.stderr }`);
+    throw ex;
+  }
+}
+
+async function kubectl(...args: string[] ): Promise<string> {
+  return await tool('kubectl', ...args);
+}
 
 describe('Rancher Desktop', () => {
   let app: Application;
-  let client: SpectronClient;
-  let browserWindow: BrowserWindow;
   let navBarPage: NavBarPage;
 
   beforeAll(async() => {
@@ -25,29 +46,37 @@ describe('Rancher Desktop', () => {
       // 'any' typing is required for now as other alternate usage/import
       //  cause issues running the tests. Without 'any' typescript
       //  complains of type mismatch.
-      path: electronPath as any,
-      args: [path.dirname(__dirname)],
+      path:                   electronPath as unknown as string,
+      args:                   [path.dirname(__dirname)],
+      // In GitHub Actions, it can take very long for things to start.
+      connectionRetryTimeout: 60_000,
     });
 
     await app.start();
-    client = app.client;
-    browserWindow = app.browserWindow;
-
     navBarPage = new NavBarPage(app);
   });
 
   afterAll(async() => {
-    if (app && app.isRunning()) {
-      await app.stop();
+    if (!app?.isRunning()) {
+      console.error('afterAll: app is not running');
+
+      return;
     }
+
+    // Due to graceful Kubernetes shutdown, we need to try to quit harder.
+    // The actual object here doesn't match the TypeScript definitions.
+    const remoteApp = (app.electron as any).remote.app;
+
+    await remoteApp.quit() as Promise<void>;
+    await app.stop();
   });
 
   it('opens the window', async() => {
-    await client.waitUntilWindowLoaded();
+    await app.client.waitUntilWindowLoaded();
 
     // Wait till the window is fully loaded n till it gets the title 'Rancher Desktop'
     for (let i = 0; i < 10; i++) {
-      const windowTitle = (await browserWindow.getTitle()).trim();
+      const windowTitle = (await app.browserWindow.getTitle()).trim();
 
       if (windowTitle === 'Rancher Desktop') {
         break;
@@ -59,7 +88,7 @@ describe('Rancher Desktop', () => {
     // Also, Alternate ways to get the app window title, for example using client
     // didn't work. So, Leaving 'await' for now. We may need to review this and
     // fix this in future.
-    const title = await browserWindow.getTitle();
+    const title = await app.browserWindow.getTitle();
 
     expect(title).toBe('Rancher Desktop');
   });
@@ -99,5 +128,71 @@ describe('Rancher Desktop', () => {
 
     expect(troubleShootingPage).not.toBeNull();
     expect(await troubleShootingPage?.getFactoryResetButtonText()).toBe('Factory Reset');
+  });
+
+  it('should run Kubernetes', async() => {
+    await app.client.waitUntilWindowLoaded();
+    const progress = await app.client.$('.progress');
+    // Wait for the progress bar to exist
+    await progress.waitForExist({ timeout: 15_000 });
+    // Wait for progress bar to disappear again
+    await progress.waitForExist({ timeout: 600_000, reverse: true });
+
+    const output = await kubectl('cluster-info');
+    // Filter out ANSI escape codes (colours).
+    const filteredOutput = output.replaceAll(/\033\[.*?m/g, '');
+
+    expect(filteredOutput).toMatch(/ is running at ./);
+  });
+
+  it('should deploy nginx server sample', async() => {
+    // let response;
+
+    try {
+      // Create namespace
+      await kubectl('create', 'namespace', 'rd-nginx-demo');
+
+      // Check that the namespace is created;
+      const namespaces = (await kubectl('get', 'namespace', '--output=name')).trim();
+      const filteredNamespaces = namespaces.replaceAll(/\033\[.*?m/g, '');
+
+      expect(filteredNamespaces).toContain('rd-nginx-demo');
+
+      // Apply resource
+      const yamlFilePath = path.join(path.dirname(__dirname), 'e2e', 'assets', 'nginx-deployment-sample', 'nginx-app.yaml');
+
+      await kubectl('apply', '-f', yamlFilePath, '-n', 'rd-nginx-demo');
+      for (let i = 0; i < 10; i++) {
+        const podName = (await kubectl('get', 'pods', '--output=name', '-n', 'rd-nginx-demo')).trim();
+
+        if (podName) {
+          expect(podName).not.toBeFalsy();
+          break;
+        }
+        await util.promisify(setTimeout)(5_000);
+      }
+      await kubectl('wait', '--for=condition=ready', 'pod', '-l', 'app=nginx', '-n', 'rd-nginx-demo', '--timeout=120s');
+
+      if (os.platform().startsWith('win')) {
+        // Forward port via UI button click, and capture the port number
+        const portForwardingPage = await navBarPage.getPortForwardingPage();
+        const port = await portForwardingPage?.portForward();
+
+        // Access app and check the welcome message
+        const response = await fetch(`http://localhost:${ port }`);
+
+        expect(response.ok).toBeTruthy();
+        response.text().then((text) => {
+          expect(text).toContain('Welcome to nginx!');
+        });
+      }
+    } finally {
+      // Delete namespace
+      await kubectl('delete', 'namespace', 'rd-nginx-demo');
+      const namespaces = (await kubectl('get', 'namespace', '--output=name')).trim();
+      const filteredNamespaces = namespaces.replaceAll(/\033\[.*?m/g, '');
+
+      expect(filteredNamespaces).not.toContain('rd-nginx-demo');
+    }
   });
 });
